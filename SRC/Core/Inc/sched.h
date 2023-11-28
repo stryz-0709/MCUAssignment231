f492e2467d55f7ee619ad1b425daecab3bf25bbb
kthkhanhtrinh <khanhtrinh81103@gmail.com>
2023-11-28 15:22:51 +0700

    Sched init

---
+++ SRC/Core/Inc/sched.h
@@ -0,0 +1,13 @@
+/*
+ * sched.h
+ *
+ *  Created on: 28 Nov 2023
+ *      Author: khanh
+ */
+
+#ifndef INC_SCHED_H_
+#define INC_SCHED_H_
+
+#include "global.h"
+
+#endif /* INC_SCHED_H_ */
---
+++ SRC/Core/Src/sched.c
@@ -0,0 +1,10 @@
+/*
+ * sched.c
+ *
+ *  Created on: 28 Nov 2023
+ *      Author: khanh
+ */
+
+#include "sched.h"
+
+
