cf6b34134c62d5495781900172894f10c3bfff4f
kthkhanhtrinh <khanhtrinh81103@gmail.com>
2023-11-28 14:19:06 +0700

    uart display

--- SRC/Core/Src/uart_timer.c
+++ SRC/Core/Src/uart_timer.c
@@ -6,24 +6,26 @@
  */
 
 #include "uart_timer.h"
-
+char* str;
 
 void displayUART(int mode, int num){
 	switch (mode){
 	case AUTO_MODE:
 		//TODO
-
+		HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!7SEG=%d#\n\r", num), 12);
 		break;
 	case RED_MODE:
 		//TODO
-
+		HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!RED=%d#\n\r", num), 12);
 		break;
 	case AMBER_MODE:
 		//TODO
+		HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!AMBER=%d#\n\r", num), 12);
 
 		break;
 	case GREEN_MODE:
 		//TODO
+		HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!GREEN=%d#\n\r", num), 12);
 
 		break;
 	}
