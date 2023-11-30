1f4c60b61afb2d2717b6fbfb2a64daa14a89b86f
stryz_0709 <minhtriinh123@gmail.com>
2023-11-30 14:46:44 +0700

    update variables

--- SRC/Core/Src/uart_timer.c
+++ SRC/Core/Src/uart_timer.c
@@ -10,20 +10,29 @@ char str[20];
 
 void displayUART(int mode, int num){
 	switch (mode){
-		case AUTO_MODE:
+		case COUNTDOWN:
 			if (num != 0) HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!7SEG=%d#\n\r", num), 1000);
 			break;
-		case RED_MODE:
+		case RED:
 			HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!RED=%d#\n\r", num), 1000);
 			break;
-		case AMBER_MODE:
+		case AMBER:
 			HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!AMBER=%d#\n\r", num), 1000);
 			break;
-		case GREEN_MODE:
+		case GREEN:
 			HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!GREEN=%d#\n\r", num), 1000);
 			break;
 		case SAVED:
 			HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!SAVED#\n\r"), 1000);
+			break;
+		case MANUAL:
+			HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!MANUAL#\n\r"), 1000);
+			break;
+		case TUNING:
+			HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!TUNING#\n\r"), 1000);
+			break;
+		case AUTO:
+			HAL_UART_Transmit(&huart2, (void*) str, sprintf(str, "!AUTO#\n\r"), 1000);
 			break;
 		default:
 			break;
